3ef0a0a6c6ececce71fc9e2b599bdd3f030dcdd3
Timothy Portfolio <tportfolio@ucla.edu>
2015-10-27 23:17:46 -0700

    added lobby settings activity

--- encore_android/app/src/main/java/cs130project/encore/LobbySettings.java
+++ encore_android/app/src/main/java/cs130project/encore/LobbySettings.java
@@ -1,6 +1,5 @@
 package cs130project.encore;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,21 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class findorcreatelobby extends AppCompatActivity {
+public class LobbySettings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_findorcreatelobby);
+        setContentView(R.layout.activity_lobby_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        }
 
-    public void enterLobby(View view) {
-        Intent intent = new Intent(this, LobbySettings.class);
-        startActivity(intent);
     }
 
 }
